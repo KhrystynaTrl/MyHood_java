4002445d79e82c8fe0810955cd74031473c365ff
Khrystyna <khrystyna.terletska@outlook.it>
2025-11-05 12:05:35 +0100

    initial commit

---
+++ src/main/java/it/start2impact/MyHood/MyHoodApplication.java
@@ -0,0 +1,13 @@
+package it.start2impact.MyHood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/start2impact/MyHood/MyHoodApplicationTests.java
@@ -0,0 +1,13 @@
+package it.start2impact.MyHood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
