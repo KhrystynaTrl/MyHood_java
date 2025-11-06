bfc2fc181b4e5421f0ae71c3bbe8fb6e4f22040e
Khrystyna <khrystyna.terletska@outlook.it>
2025-11-06 13:30:16 +0100

    BCrypt bean

--- src/main/java/it/start2impact/MyHood/MyHoodApplication.java
+++ src/main/java/it/start2impact/MyHood/MyHoodApplication.java
@@ -2,12 +2,21 @@ package it.start2impact.MyHood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"it.start2impact.MyHood"})
 public class MyHoodApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyHoodApplication.class, args);
 	}
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
