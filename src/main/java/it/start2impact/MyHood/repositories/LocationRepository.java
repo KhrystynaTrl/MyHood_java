aba45530cd50c94a06e3d4c653728ce36dfaabb8
Khrystyna <khrystyna.terletska@outlook.it>
2025-11-08 18:43:23 +0100

    findByLocation

--- src/main/java/it/start2impact/MyHood/repositories/LocationRepository.java
+++ src/main/java/it/start2impact/MyHood/repositories/LocationRepository.java
@@ -4,6 +4,10 @@ import it.start2impact.MyHood.entities.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocationRepository extends JpaRepository<LocationEntity, Integer> {
+
+    Optional<LocationEntity> findByLocationIgnoreCase(String location);
 }
